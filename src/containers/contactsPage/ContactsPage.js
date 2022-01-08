2f55d55b649dd6a9471b8c6951294013d54ffd4d
Damian Kozyra <d.kozyra@hotmail.co.uk>
2022-01-08 23:50:30 +0000

    Complete contact page

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import { ContactForm } from '../../components/contactForm/ContactForm';
+import { TileList } from '../../components/tileList/TileList';
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const contacts = props.contacts;
+  const addContact = props.addContact;
+
+  // Keep track of three local state values
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+
+  // Check for duplicates
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,21 +25,49 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (!duplicate) {
+      addContact(name, phone, email);
+      
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(() => {
+    for (const contact of contacts) {
+      if (name === contact.name) {
+        setDuplicate(true);
+      }
+
+      return;
+    }
+  });
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm 
+          name={name}
+          phone={phone}
+          email={email}
+          setName={setName}
+          setPhone={setPhone}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList
+          contactsArr={contacts}
+        />
       </section>
     </div>
   );
