8e422180705849abcc440c8235075aab16aac263
Damian Kozyra <d.kozyra@hotmail.co.uk>
2022-01-09 21:10:59 +0000

    complete tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (value) => {
+  const arr = Object.values(value);
   return (
     <div className="tile-container">
-      
+      {arr.map( (data, index) => {
+        if (index === 0 ) {
+          return <p className="tile-title" key={index}>{data}</p>;
+        }
+          return <p className="tile" key={index}>{data}</p>;
+        })
+      }
     </div>
   );
 };
