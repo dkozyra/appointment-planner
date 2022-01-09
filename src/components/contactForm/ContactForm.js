2569751023dfbd9bae1fd10068e7542de3c37fec
Damian Kozyra <d.kozyra@hotmail.co.uk>
2022-01-09 20:08:12 +0000

    complete contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,32 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        value={name}
+        onChange={({target}) => {setName(target.value)}}
+        placeholder="Contact Name"
+        required
+      />
+      <input 
+        type="tel"
+        value={phone}
+        pattern="^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$"
+        onChange={({target}) => {setPhone(target.value)}}
+        placeholder="Contact Phone"
+        required
+      />
+      <input 
+        type="email"
+        value={email}
+        onChange={({target}) => {setEmail(target.value)}}
+        placeholder="Contact Email"
+        required
+      />
+      <input
+        type="submit"
+      />
+    </form>
   );
 };
