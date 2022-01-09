7ae1a2be8fffb957dcbf16a26b727a5cd94d7dfa
Damian Kozyra <d.kozyra@hotmail.co.uk>
2022-01-09 22:29:49 +0000

    complete contact picker plus small other fixes

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const Tile = (value) => {
+export const Tile = ({value}) => {
   const arr = Object.values(value);
   return (
     <div className="tile-container">
-      {arr.map( (data, index) => {
+      {arr.map((data, index) => {
         if (index === 0 ) {
           return <p className="tile-title" key={index}>{data}</p>;
         }
